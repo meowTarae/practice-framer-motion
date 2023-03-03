4a0179d38527a13ac485eaed473d13aa543b26ac
타래 <85001878+meowTarae@users.noreply.github.com>
2023-03-04 00:07:59 +0900

    Practice framer-motion (fin)

--- src/App.tsx
+++ src/App.tsx
@@ -1,52 +1,152 @@
 import styled from "styled-components";
-import { motion } from "framer-motion";
+import { motion, AnimatePresence } from "framer-motion";
+import { useState } from "react";
 
-const Wrapper = styled(motion.div)`
+const Wrapper = styled.div`
   height: 100vh;
   width: 100vw;
+  display: flex;
+  /* flex-direction: column; */
+  justify-content: center;
+  align-items: center;
+
+  position: relative;
+`;
+
+const Container = styled.div`
+  display: grid;
+  grid-template-columns: 20vw 20vw;
+  grid-template-rows: 200px 200px;
+  grid-gap: 12px;
+`;
+
+const Box = styled(motion.div)`
+  border-radius: 10px;
+  /* height: 200px; */
+  box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
+  background: rgba(255, 255, 255, 0.3);
+  -webkit-backdrop-filter: blur(10px);
+  backdrop-filter: blur(10px);
+  border: 1px solid rgba(255, 255, 255, 0.25);
+
   display: flex;
   justify-content: center;
   align-items: center;
 `;
 
-const Svg = styled.svg`
-  width: 300px;
-  height: 300px;
-  path {
-    stroke: white;
-    stroke-width: 2;
-  }
+const InnerBox = styled(motion.div)`
+  border-radius: 10px;
+  height: 200px;
+  box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
+  background: rgba(255, 255, 255, 1);
+  backdrop-filter: blur(10px);
+  border: 1px solid rgba(255, 255, 255, 0.25);
+
+  display: flex;
+  justify-content: center;
+  align-items: center;
 `;
 
-const svg = {
-  start: { pathLength: 0, fill: "rgba(255, 255, 255, 0)" },
-  end: {
-    fill: "rgba(255, 255, 255, 1)",
-    pathLength: 1,
-  },
+const Overlay = styled(motion.div)`
+  width: 100%;
+  height: 100%;
+  position: absolute;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const overlay = {
+  hidden: { backgroundColor: "rgba(0, 0, 0, 0)" },
+  visible: { backgroundColor: "rgba(0, 0, 0, 0.5)" },
+  exit: { backgroundColor: "rgba(0, 0, 0, 0)" },
 };
 
+const Ball = styled(motion.div)`
+  width: 50px;
+  height: 50px;
+  border-radius: 50%;
+  border: 8px solid #8c7ae6;
+  background-color: transparent;
+  box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
+`;
+
+const Button = styled(motion.button)`
+  background-color: white;
+  margin-top: 4%;
+  position: absolute;
+  bottom: 24%;
+  width: 72px;
+  height: 36px;
+  border: 1px solid transparent;
+  border-radius: 4px;
+  padding: 0 15px;
+  cursor: pointer;
+  font-size: 14px;
+`;
+
 function App() {
+  const [id, setId] = useState<null | string>(null);
+  const [click, setClick] = useState(true);
+  const onClick = () => {
+    setClick((prev) => !prev);
+  };
   return (
     <Wrapper>
-      <Svg
-        focusable="false"
-        xmlns="http://www.w3.org/2000/svg"
-        viewBox="0 0 448 512"
+      <Container>
+        {["1", "2", "3", "4"].map((n) =>
+          parseInt(n) === 2 ? (
+            <Box onClick={() => setId(n)} key={n} layoutId={n}>
+              {click ? <Ball layoutId="ball" /> : null}
+            </Box>
+          ) : parseInt(n) === 3 ? (
+            <Box onClick={() => setId(n)} key={n} layoutId={n}>
+              {!click ? <Ball layoutId="ball" /> : null}
+            </Box>
+          ) : (
+            <Box
+              onClick={() => setId(n)}
+              key={n}
+              layoutId={n}
+              whileHover={{
+                margin:
+                  parseInt(n) === 1 ? "-20px 0 0 -20px" : "0 -20px -20px 0",
+              }}
+            />
+          )
+        )}
+      </Container>
+
+      <AnimatePresence>
+        {id ? (
+          <Overlay
+            variants={overlay}
+            onClick={() => setId(null)}
+            initial="hidden"
+            animate="visible"
+            exit="exit"
+          >
+            <InnerBox layoutId={id} style={{ width: 400, height: 200 }}>
+              {(parseInt(id) === 2 && click) ||
+              (parseInt(id) === 3 && !click) ? (
+                <Ball layoutId="ball" />
+              ) : null}
+            </InnerBox>
+          </Overlay>
+        ) : null}
+      </AnimatePresence>
+
+      <Button
+        onClick={onClick}
+        style={{
+          color: click ? "skyblue" : "orange",
+        }}
+        animate={{ scale: !click ? 1.2 : 1 }}
       >
-        <motion.path
-          variants={svg}
-          initial="start"
-          animate="end"
-          transition={{
-            default: { duration: 5 },
-            fill: { duration: 1, delay: 3 },
-          }}
-          d="M224 373.12c-25.24-31.67-40.08-59.43-45-83.18-22.55-88 112.61-88 90.06 0-5.45 24.25-20.29 52-45 83.18zm138.15 73.23c-42.06 18.31-83.67-10.88-119.3-50.47 103.9-130.07 46.11-200-18.85-200-54.92 0-85.16 46.51-73.28 100.5 6.93 29.19 25.23 62.39 54.43 99.5-32.53 36.05-60.55 52.69-85.15 54.92-50 7.43-89.11-41.06-71.3-91.09 15.1-39.16 111.72-231.18 115.87-241.56 15.75-30.07 25.56-57.4 59.38-57.4 32.34 0 43.4 25.94 60.37 59.87 36 70.62 89.35 177.48 114.84 239.09 13.17 33.07-1.37 71.29-37.01 86.64zm47-136.12C280.27 35.93 273.13 32 224 32c-45.52 0-64.87 31.67-84.66 72.79C33.18 317.1 22.89 347.19 22 349.81-3.22 419.14 48.74 480 111.63 480c21.71 0 60.61-6.06 112.37-62.4 58.68 63.78 101.26 62.4 112.37 62.4 62.89.05 114.85-60.86 89.61-130.19.02-3.89-16.82-38.9-16.82-39.58z"
-        ></motion.path>
-      </Svg>
+        Switch
+      </Button>
     </Wrapper>
   );
 }
 
-export default App
+export default App;
